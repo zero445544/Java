95c61f6a04aaa5a15803bff5b76497d42d4185fb
Steven-PC <zero445544>
2020-10-15 14:02:19 -0500

    2

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -3,11 +3,7 @@ package helloworld;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        String message = "  Hello World" + "!!  ";
-        
-        System.out.println(message.replace("!" , "*"));
-        System.out.println(message.trim());
-        System.out.println(message);
+        System.out.println("Hello World");
     }
      
 }
